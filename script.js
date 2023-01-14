2a7487cdedf8cd49991e11ed0695d23aa663cf87
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-01-14 16:21:31 -0500

    d

--- script.js
+++ script.js
@@ -6,13 +6,18 @@ window.onfocus = function() {
     document.title = "Esmél"
 }
 
-function cancel() {
-    document.getElementById("privacy").style.display = "none";
-    document.getElementById("maincontent").style.display = "block";
-    window.open('', '_blank');
-  }
+// function cancel() {
+//     document.getElementById("privacy").style.display = "none";
+//     document.getElementById("maincontent").style.display = "block";
+//     window.open('', '_blank');
+//   }
   
-  function accept() {
-    document.getElementById("privacy").style.display = "none";
-    document.getElementById("maincontent").style.display = "block";
-  }
+//   function accept() {
+//     document.getElementById("privacy").style.display = "none";
+//     document.getElementById("maincontent").style.display = "block";
+//   }
+
+window.onload = function() {
+    alert('I take the security and protection of our visitors data very seriously. I understand that trust is essential in any online relationship, and I am committed to ensuring that your visit is secure.<br>I take the security and protection of our visitors data very seriously. I understand that trust is essential in any online relationship, and I am committed to ensuring that your visit is secure.<br><br> To accomplish this, I have implemented a number of security measures to ensure that your visit is secure at all times. Some of these measures include:<br><ul><li>SSL/TLS encryption: All data transmitted to and from my website is encrypted using SSL/TLS technology, which ensures that your visit is protected from unauthorized access.</li><li>HSTS encryption: With HSTS, your browser will automatically upgrade the connection to HTTPS, making it more difficult for a hacker to intercept the connection.</li><li>Regular security updates: I regularly update my website to ensure that I am always using the latest security features and patches where available.</li></ul><br>I understand that no system is completely invulnerable to cyber threats, but I do everything in my power to protect your visit. I encourage you to take precautions to protect your personal information as well such as using a VPN or Proxy. If you have any concerns about our security measures, please contact us at security@esmel.xyz.<br>Thank you for visitng and I assure you that we have taken all the necessary precautions to protect your connecting and visit.');
+}
+
